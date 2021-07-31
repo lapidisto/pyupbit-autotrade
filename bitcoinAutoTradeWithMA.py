a386b8dbb5293e54534c4316257c9e0de525b11a
lapidisto <88225963+lapidisto@users.noreply.github.com>
2021-08-01 02:33:52 +0900

    Update bitcoinAutoTradeWithMA.py

--- bitcoinAutoTradeWithMA.py
+++ bitcoinAutoTradeWithMA.py
@@ -50,17 +50,17 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
+            target_price = get_target_price("KRW-BTC", 0.1)
             ma15 = get_ma15("KRW-BTC")
             current_price = get_current_price("KRW-BTC")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    upbit.buy_market_order("KRW-BTC", krw*0.4995)
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                upbit.sell_market_order("KRW-BTC", btc*0.4995)
         time.sleep(1)
     except Exception as e:
         print(e)
